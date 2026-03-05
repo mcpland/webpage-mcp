94faf5ae4a638bc6e87c9345b0b205fe332cf922
unadlib <unadlib@gmail.com>
2026-03-06 01:13:07 +0800

    chore(scope): remove unused popup schedule dialog component

--- app/chrome-extension/entrypoints/popup/react/components/index.ts
+++ app/chrome-extension/entrypoints/popup/react/components/index.ts
@@ -2,4 +2,3 @@ export * from "./ConfirmDialog";
 export * from "./ElementMarkerManagement";
 export * from "./ModelCacheManagement";
 export * from "./ProgressIndicator";
-export * from "./ScheduleDialog";
